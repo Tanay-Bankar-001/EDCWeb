0014aeb951f77e2c9763a95f365fb6f5294b4915
Tanay-Bankar-001 <tanaybankar001@gmail.com>
2025-05-31 19:12:19 +0530

    Hero, wavy boundary and logo

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -65,21 +65,22 @@ const Hero: React.FC = () => {
       </div>
       
       {/* Decorative Wave */}
-      <div className="absolute bottom-0 left-0 right-0">
-        <svg className="w-full h-auto" viewBox="0 0 1440 120" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M0 120L60 110C120 100 240 80 360 70C480 60 600 60 720 65C840 70 960 80 1080 85C1200 90 1320 90 1380 90L1440 90V120H1380C1320 120 1200 120 1080 120C960 120 840 120 720 120C600 120 480 120 360 120C240 120 120 120 60 120H0V120Z" fill="white"/>
-        </svg>
-        <div className="absolute left-1/2 transform -translate-x-1/2 -translate-y-full">
-          <motion.img
-            initial={{ y: 20, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ duration: 0.6, delay: 0.6 }}
-            src="https://raw.githubusercontent.com/Tanay-Bankar-001/ImagesEDC/main/Logos/ed_cell_logo-removebg-preview.png"
-            alt="ED Cell Logo"
-            className="w-32 h-32 object-contain"
-          />
-        </div>
-      </div>
+<div className="absolute bottom-0 left-0 right-0">
+  <svg className="w-full h-auto" viewBox="0 0 1440 120" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M0 120L60 110C120 100 240 80 360 70C480 60 600 60 720 65C840 70 960 80 1080 85C1200 90 1320 90 1380 90L1440 90V120H1380C1320 120 1200 120 1080 120C960 120 840 120 720 120C600 120 480 120 360 120C240 120 120 120 60 120H0V120Z" fill="white"/>
+  </svg>
+  <div className="absolute left-1/2 transform -translate-x-1/2 -translate-y-2/3">
+    <motion.img
+      initial={{ y: 20, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.6, delay: 0.6 }}
+      src="https://raw.githubusercontent.com/Tanay-Bankar-001/ImagesEDC/main/Logos/ed_cell_logo-removebg-preview.png"
+      alt="ED Cell Logo"
+      className="w-32 h-32 object-contain drop-shadow-xl"
+    />
+  </div>
+</div>
+
     </section>
   );
 };
